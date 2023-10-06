a0d32a70839dd518eef99d918a88d076b0c030d7
Michael <michaelnjoku1200@gmail.com>
2023-10-06 10:34:54 +0100

    start of the bot. can accept cookies and enter click the text bar

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import selenium
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
@@ -5,40 +7,65 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+email = "michaelnjoku1200@gmail.com"
+password = "Xtreme32"
+typing_delay = 0.25
 # Create a Safari WebDriver
 driver = selenium.webdriver.Safari()
 
+# Maximize the Safari window
+driver.maximize_window()
+
 # Use the driver to automate Safari
-# Open the Nike website
-driver.get("https://www.nike.com")
+# Open the ticketmaster website
+driver.get("https://www.ticketmaster.ie")
 driver.implicitly_wait(5)
 
-
 try:
-    wait = WebDriverWait(driver, 15)
-    accept_all_cookies_button = driver.find_element(By.CSS_SELECTOR, ".nds-btn.dialog-actions-accept-btn.btn-primary-dark.btn-md")
-    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".nds-btn.dialog-actions-accept-btn.btn-primary-dark.btn-md")))
-    accept_all_cookies_button.click()
-
-    # Find the search bar element, wait 20 seconds for the search bar element to be clickable
-
-    search_bar = driver.find_element(By.CLASS_NAME, "pre-search-btn")
-
-    wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "pre-search-btn")))
-
-    search_bar.click()
-
-    input_box = driver.find_element(By.ID, "VisualSearchInput")
-
-    # Send keys to the search bar
-    search_bar.send_keys("air force 1")
-
-    # Submit the search query (press Enter)
-    search_bar.send_keys(Keys.RETURN)
+    time.sleep(2)
+    # reject_cookies = driver.find_element(By.ID, "onetrust-reject-all-handler")
+    accept_cookies = driver.find_element(By.ID ,"onetrust-accept-btn-handler")
+   # reject_cookies.click()
+    accept_cookies.click()
 
 except NoSuchElementException:
-    # Handle the case where the button is not found
-    print("Button not found. Performing alternative action or error handling.")
+    print("Element not found")
+try:
+    login = driver.find_element(By.CLASS_NAME,"sc-6wpn76-1")
 
+    login.click()
+
+except NoSuchElementException:
+    print("Element not found")
+
+time.sleep(5)
+try:
+    enter_email = driver.find_element(By.ID, "email[objectobject]__input")
+
+    enter_email.click()
+    for char in email:
+        enter_email.send_keys(char)
+        time.sleep(typing_delay)
+
+
+    time.sleep(2)
+    enter_password = driver.find_element(By.ID,"password[objectobject]__input")
+    enter_password.click()
+    for char in password:
+        enter_password.send_keys(char)
+        time.sleep(typing_delay)
+
+
+
+    time.sleep(2)
+    remember_me = driver.find_element(By.CLASS_NAME,"sc-qYHkt")
+    remember_me.click()
+    time.sleep(4)
+    sign_in = driver.find_element(By.NAME,"sign-in")
+    sign_in.click()
+except NoSuchElementException:
+    print("element not found")
+# Wait for a few seconds (e.g., 10 seconds) to keep the browser open
+time.sleep(10)
 # Close the browser
 driver.quit()
